3a57b37ffa3f79e7e4f68cc250d6893373d835c5
Yash Gosavi <iyashgosavi02@gmail.com>
2024-09-25 17:41:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/twitter',(req,res)=>{
+    res.send('Twitter');
+})
+
+app.get('/login',(req,res)=>{
+    res.send('<h1>Login Successfully</h1>');
+})
+
+app.get('/chai',(req,res)=>{
+    res.send('<h3>Chai is ready</h3>');
+})
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);           
+});
